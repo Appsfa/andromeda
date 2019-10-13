1dc9a5877c9380567bd35a0254132438e5d7a0bc
Fabián Moreno Islas <fabianislas@apps-fa.com>
2019-10-13 15:19:42 -0500

    Probando users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+const User = require('../models/user');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  User.find({})
+    .then(result => {
+      if (result.length) {
+          res.status(200).json({
+            user:result
+          });
+      }else {
+          res.status(404).send('Aqui no hay usuarios');
+      }
+    })
+    .catch(next)
 });
-
 module.exports = router;
