3899c956842f030095035b826280fe71b4fb12b4
Fabián Moreno Islas <fabianislas@apps-fa.com>
2019-11-17 21:02:42 -0600

    make it better

--- app.js
+++ app.js
@@ -27,6 +27,11 @@ mongoose.connect('mongodb+srv://appsfa:Trainedcrib1@cluster0-8h29p.mongodb.net/t
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
