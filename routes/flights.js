75187bc98e1b3a7d9c1a4c7ad400896f167c9525
Fabián Moreno Islas <fabianislas@apps-fa.com>
2019-11-23 09:33:45 -0600

    cors

--- routes/flights.js
+++ routes/flights.js
@@ -69,7 +69,7 @@ router.get('/:id', (req, res, next) =>{
 
 router.get('/spaceship/:id', (req, res, next) =>{
   let id = req.params.id;
-  Flights.findOne({ idSpaceship: id }).exec()
+  Flights.find({ idSpaceship: id }).exec()
       .then(result => {
         if(result){
           res.status(200).json({
