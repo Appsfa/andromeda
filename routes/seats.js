feafb099441f03a4db451cda6b2588ed9587498a
Fabián Moreno Islas <fabianislas@apps-fa.com>
2019-11-23 14:46:05 -0600

    cors

--- routes/seats.js
+++ routes/seats.js
@@ -67,7 +67,7 @@ router.get('/:id', (req, res, next) =>{
 });
 
 
-router.get('user/:id', (req, res, next) =>{
+router.get('/user/:id', (req, res, next) =>{
   let id = req.params.id;
   Seats.find( {username: id} ).exec()
       .then(result => {
